30f135b4553e0559c0b5f872ae38f9f801e60147
flurbudurbur <sebastianhoogendijk@gmail.com>
2022-06-11 01:04:42 +0200

    imma go to bed

--- index.php
+++ index.php
@@ -5,14 +5,33 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="icon" type="image/x-icon" href="assets/images/logo.svg">
+    <script src="assets/js/active.js"></script>
     <title>Document</title>
 </head>
 <body>
-    <header class=""><nav>
-        <ul>
-            <li><a href="#">here's some text</a></li>
-            <li><a href="#"></a></li>
-            <li><a href="#"></a></li>
+<script>0</script> <!-- Firefox hack, lemme be! -->
+    <header class="header"><nav class="topNav">
+        <img class="logo" src="assets/images/logo.svg" alt="">
+        <ul id="navHeader">
+            <li class="navListItem">
+                <div class="navButtonWrapper">
+                    <img class="navIcons" src="assets/images/icons/folder-solid.svg" alt="">
+                    <a class="navBtn" href="#">Projects</a>
+                </div>
+                <span class="underline"></span></li>
+            <li class="navListItem">
+                <div class="navButtonWrapper">
+                    <img class="navIcons" src="assets/images/icons/user-solid.svg" alt="">
+                    <a class="navBtn" href="#">About</a>
+                </div>
+                <span class="underline"></span></li>
+            <li class="navListItem active">
+                <div class="navButtonWrapper">
+                    <img class="navIcons" src="assets/images/icons/house-solid.svg" alt="">
+                    <a class="navBtn" href="#">Home</a>
+                </div>
+                <span class="underline"></span></li>
         </ul>
     </nav></header>
     <main>
