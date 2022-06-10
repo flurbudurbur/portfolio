741cfe2a227b08a8c1e441ce72d47c1c5f2b556b
flurbudurbur <sebastianhoogendijk@gmail.com>
2022-06-10 17:15:58 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header class=""><nav>
+        <ul>
+            <li><a href="#">here's some text</a></li>
+            <li><a href="#"></a></li>
+            <li><a href="#"></a></li>
+        </ul>
+    </nav></header>
+    <main>
+        <div>
+            <h1>Sebastian Hoogendijk</h1>
+            <p>Software developer with focus on front-end development.</p>
+            <a href="#">Learn more</a>
+        </div>
+        <img src="assets/images/code.png" alt="website code from a cool perspective">
+    </main>
+</body>
+</html>
